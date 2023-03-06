1166dc27dd97e7c72c8cab6f06e899d3d73a0f54
cromeoli <cromero@iesrafaelalberti.es>
2023-03-06 06:42:32 +0100

    Pantallita de comingsoon hecha

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,17 @@
 import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 
 import Feed from "../components/Feed/Feed.vue";
+import ComingSoon from "../components/ComingSoon.vue";
 
 const routes = [
 
     {
         path:'/',
-        component:Feed
+        component:Feed,
+    },
+    {
+        path: '/comingSoon',
+        component: ComingSoon,
     }
 ]
 
